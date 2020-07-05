e110401f1d746dfe1c77804cb4cbf89d1b72acab
technobird22 <51074275+kiwibird22@users.noreply.github.com>
2020-07-05 22:26:33 +1200

    Removed unused code and fixed overlay
    Removed unused code and fixed overlay

--- geocap_utils.py
+++ geocap_utils.py
@@ -5,31 +5,29 @@ img_name = "FD8_IMG_FD_125_IR105_20200704_210006.jpg"
 img = cv2.imread(path + img_name, 1)
 
 def parse_frame_name(input_name):
-    # "FD8_IMG_FD_125_IR105_20200704_210006.jpg" to "125: 20200704 21:00:06" 
-    # output_name = input_name[input_name.find("FD_"):].replace("FD_", "")
     output = input_name[:-6].split('_')
 
     num = output[3]
     date, time = output[5:7]
 
-    # date = list(date)
     date = date[6:8] + "/" + date[4:6] + "/" + date[0:4]
     time = time[0:2] + ":" + time[2:5]
 
-    output_name = date + " @ " + time + " UTC"
+    output_name = date + " @ " + time + "UTC"
     return (output_name, num)
 
-def overlay_text(frame_name):
-    frame_time, frame_num = parse_name(frame_name)
-
+def overlay_text(inp_img, frame_name):
     print("[INFO] Overlaying text onto frame...")
+
+    frame_time, frame_num = parse_frame_name(frame_name)
+
     font = cv2.FONT_HERSHEY_PLAIN
     cv2.putText(img, "GK-2A Imagery", (20, 80), font, 5, (0, 255, 255), 5, cv2.LINE_AA)
 
     cv2.putText(img, "Animation", (20, 170), font, 6, (0, 165, 255), 5, cv2.LINE_AA)
 
     cv2.putText(img, "Capture Time: ", (1720, 60), font, 4, (0, 255, 255), 4, cv2.LINE_AA)
-    cv2.putText(img, frame_time, (1600, 110), font, 3, (150, 150, 150), 3, cv2.LINE_AA)
+    cv2.putText(img, frame_time, (1570, 110), font, 3, (150, 150, 150), 3, cv2.LINE_AA)
 
     cv2.putText(img, "Frame Number: ", (1690, 180), font, 4, (0, 255, 255), 4, cv2.LINE_AA)
     cv2.putText(img, frame_num, (2070, 240), font, 4, (150, 150, 150), 4, cv2.LINE_AA)
@@ -46,17 +44,21 @@ def overlay_text(frame_name):
 
     cv2.putText(img, "Thanks :)", (2100, 2194), font, 1, (50, 50, 50), 1, cv2.LINE_AA)
 
+    return(img)
+
 print("-"*30)
 print("---START---")
 
-inp = "FD8_IMG_FD_125_IR105_20200704_210006.jpg"
-print("Input name:", inp)
-out = parse_frame_name(inp)[0]
+print("Input name:", img_name)
+out = parse_frame_name(img_name)[0]
 print("Output time:", out)
-num = parse_frame_name(inp)[1]
+num = parse_frame_name(img_name)[1]
 print("Frame number:", num)
+
+img = overlay_text(img, img_name)
 
 print("---DONE---")
 print("-"*30)
+
 # Write final output image
 cv2.imwrite(path + "text_" + img_name, img)
