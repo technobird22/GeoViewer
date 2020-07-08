23e77988644cf946c13d27796f15b2b5f6eb79fb
technobird22 <51074275+kiwibird22@users.noreply.github.com>
2020-07-08 22:48:31 +1200

    Added universal error handler to sharpen_3.py

--- sharpen_3.py
+++ sharpen_3.py
@@ -7,7 +7,12 @@ output_header = "sharpened_"
 inputs = ['test_fd.png']
 
 for cur_file in inputs:
-    sharpened = Image.open(f'{cur_file}')
+    try:
+        sharpened = Image.open(f'{cur_file}')
+    except:
+        print("[ERROR] Image '" + cur_file + "' couldn't be opened! Cause: '" + str(sys.exc_info()[0]) + "'.\n\
+        Skipping to next image...")
+        continue
 
     for i in range(sharpenAmount):
         sharpened = sharpened.filter(ImageFilter.SHARPEN)
