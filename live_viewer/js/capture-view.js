a83a58c180b5b100a42e7274d6fb20811e58448d
technobird22 <51074275+kiwibird22@users.noreply.github.com>
2020-08-07 21:33:32 +1200

    General updates to viewer page

--- live_viewer/js/capture-view.js
+++ live_viewer/js/capture-view.js
@@ -1,23 +1,24 @@
-function change_enhancement(filter){
+function change_image(img){
     var pass = "2020-06-01-21-35-48-NOAA_18";
     var image_directory = "images/page_for_all/";
     
-    var path = image_directory + pass + "-" + filter + ".jpg";
-    var tnpath = image_directory + pass + "-" + filter + "-tn.jpg";
-    // var path = "images/page_for_all/".concat(pass, "-", filter, ".jpg");
-    // alert("Changing Filter...\nFilter Requested: " + filter + "\nChanging image display source to:\n" + path);
+    var path = image_directory + pass + "-" + img + ".jpg";
+    var tnpath = image_directory + pass + "-" + img + "-tn.jpg";
+    // var path = "images/page_for_all/".concat(pass, "-", img, ".jpg");
+    // alert("Changing Filter...\nFilter Requested: " + img + "\nChanging image display source to:\n" + path);
     document.getElementById("display").src = path;
     document.getElementById("description").innerHTML = 
-    "Pass ID: <br><span class=\"param\">" + pass + "</span>" + 
-    "<hr>About this filter: <br><span class=\"param\">" + about_filter(filter) + "</span>" + 
-    "<hr>Path to filter image: <br><span class=\"param\">" + location.href.replace("/passes.html", "") + "/" + path + "</span>" + 
-    "<hr>Open filter image in new tab: <br><button onclick=\"window.open('" + location.href.replace("/passes.html", "") + "/" + path + "\', \'_blank\');\">" + "Original Quality" + "</button>" +
+    "Image capture time: <br><span class=\"param\">" + pass + "</span>" + 
+    "<hr>About this image: <br><span class=\"param\">" + about_img(img) + "</span>" + 
+    "<hr>Path to image: <br><span class=\"param\">" + location.href.replace("/passes.html", "") + "/" + path + "</span>" + 
+    "<hr>Open image in new tab: <br><button onclick=\"window.open('" + location.href.replace("/passes.html", "") + "/" + path + "\', \'_blank\');\">" + "Original Quality" + "</button>" +
     "<button onclick=\"window.open('" + location.href.replace("/passes.html", "") + "/" + tnpath + "\', \'_blank\');\">" + "Reduced Quality" + "</button>";
     // document.getElementById("description").innerHTML = path;
 }
 
-function about_filter(filter){
-    switch(filter){
+function about_img(img){
+    // alert(img)
+    switch(img){
         case "za":
             return "Enhanced infra red image";
         case "no":
@@ -36,6 +37,6 @@ function about_filter(filter){
             return "Contrast - Channel B image";
         
         default:
-            return "Filter not found. Please submit an issue on GitHub for Albert (technobird22)";
+            return "Requested image not found. Please submit an issue on GitHub for Albert (technobird22)";
     }
 }
