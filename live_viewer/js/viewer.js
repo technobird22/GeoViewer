0e7b024d8fef911af9e4db15d559e0cce06fd15f
technobird22 <51074275+kiwibird22@users.noreply.github.com>
2020-08-08 13:47:12 +1200

    Updates to viewer backend

--- live_viewer/js/viewer.js
+++ live_viewer/js/viewer.js
@@ -1,19 +1,18 @@
 function change_image(img){
     var image_site = "https://kiwiweather.com/";
     var image_directory = "gk-2a/";
-    var path = image_site + image_directory + img + ".jpg";
-    var fullpath = location.href.replace("/captures.html", "") + "/" + path;
-    var tnpath = image_directory + "-" + img + "-tn.jpg";
+    var path = image_site + image_directory + img;
+    var fullpath = path + ".jpg";
+    var tnpath = path + "-tn.jpg";
 
     // alert("Changing Filter...\nFilter Requested: " + img + "\nChanging image display source to:\n" + path);
-    document.getElementById("display").src = path;
+    document.getElementById("display").src = fullpath;
     document.getElementById("description").innerHTML = 
     "Image capture time: <br><span class=\"param\">" + " [PLACEHOLDER] " + "</span>" + 
     "<hr>About this image: <br><span class=\"param\">" + about_img(img) + "</span>" + 
     "<hr>Path to image: <br><span class=\"param\">" + fullpath + "</span>" + 
     "<hr>Open image in new tab: <br><button onclick=\"window.open('" + fullpath + "', '_blank');\">" + "Original Quality" + "</button>" +
-    "<button onclick=\"window.open('" + location.href.replace("/passes.html", "") + "/" + tnpath + "', '_blank');\">" + "Reduced Quality" + "</button>";
-    // document.getElementById("description").innerHTML = path;
+    "<button onclick=\"window.open('" + tnpath + "', '_blank');\">" + "Reduced Quality" + "</button>";
 }
 
 function about_img(img){
