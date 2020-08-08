9203c6ec07a3c406aa2f95b20268a341e9f9f068
technobird22 <51074275+kiwibird22@users.noreply.github.com>
2020-08-08 13:50:14 +1200

    Updated viewer backend to account for other file types

--- live_viewer/js/viewer.js
+++ live_viewer/js/viewer.js
@@ -2,16 +2,15 @@ function change_image(img){
     var image_site = "https://kiwiweather.com/";
     var image_directory = "gk-2a/";
     var path = image_site + image_directory + img;
-    var fullpath = path + ".jpg";
     var tnpath = path + "-tn.jpg";
 
     // alert("Changing Filter...\nFilter Requested: " + img + "\nChanging image display source to:\n" + path);
-    document.getElementById("display").src = fullpath;
+    document.getElementById("display").src = path;
     document.getElementById("description").innerHTML = 
     "Image capture time: <br><span class=\"param\">" + " [PLACEHOLDER] " + "</span>" + 
     "<hr>About this image: <br><span class=\"param\">" + about_img(img) + "</span>" + 
-    "<hr>Path to image: <br><span class=\"param\">" + fullpath + "</span>" + 
-    "<hr>Open image in new tab: <br><button onclick=\"window.open('" + fullpath + "', '_blank');\">" + "Original Quality" + "</button>" +
+    "<hr>Path to image: <br><span class=\"param\">" + path + "</span>" + 
+    "<hr>Open image in new tab: <br><button onclick=\"window.open('" + path + "', '_blank');\">" + "Original Quality" + "</button>" +
     "<button onclick=\"window.open('" + tnpath + "', '_blank');\">" + "Reduced Quality" + "</button>";
 }
 
@@ -19,17 +18,17 @@ function about_img(img){
     // alert(img)
     switch(img){
         // Full disk images
-        case "FD":
+        case "FD.jpg":
             return "This is the raw, unprocessed infra-red frame, as downloaded from the satellite.";
-        case "clahe":
+        case "clahe.jpg":
             return "This is the infra-red frame enhanced with CLAHE processing";
-        case "sanchez":
+        case "sanchez.jpg":
             return "This is the colourized version of the raw infra-red image";
 
         // Charts and Predictions
-        case "GWW3F":
+        case "GWW3F.gif":
             return "Global Wave Model data";
-        case "UP50A":
+        case "UP50A.gif":
             return "Pressure and temperature data for South-East Asia";
         case "sea":
             return "Sea surface temperature image";
